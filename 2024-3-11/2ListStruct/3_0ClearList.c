fa4725d1d27b39f7ed05efa434b1571bd5122788
AlonelyR <130484696+Enolaalone@users.noreply.github.com>
2024-03-11 21:16:11 +0800

    Add files via upload

---
+++ 2024-3-11/2ListStruct/3_0ClearList.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#define ListLength 100//指针数组长度
+#define OVERFLOW -2//指针数组长度
+#define LIST_SIZE 100// int* next长度
+#define Length_LIST_SIZE 10// int* next增加长度
+bool pr=false;
+
+typedef struct List{//链表结构
+    int val;
+    struct List* next;
+}List;
+
+void print(bool *r);//菜单
+bool InitList(List** head);//建立链表
+bool DestroyList(List** head);//销毁链表--2
+bool ClearList(List* head);//清空链表--3
+
+int main() {//主函数
+    char E[4] = "\0";
+    int num, l_num;
+    //初始化-------------------------------
+    List *ps[ListLength];//链表数组
+    for (int i = 0; i < ListLength; ++i) {
+        ps[i]=NULL;
+    }
+
+    while (1) {
+        printf("选择对以下哪个链表进行操作（1-99），按0退出！:\n");
+        gets(E);
+        l_num = atoi(E);
+        if (l_num == 0)break;
+        else if(l_num<=99 && l_num>=0) {
+            while (1) {
+                print(&pr);
+                gets(E);
+                num = atoi(E);
+                if (num == 0) {
+                    pr = false;
+                    break;
+                } else if (num == 1) {
+                    if (InitList(&ps[l_num]))
+                        printf("链表创建成功!\n");
+                    else
+                        printf("链表已经存在!\n");
+                }
+                else if(num==2){
+                    if (DestroyList(&ps[l_num]))
+                        printf("链表销毁成功!\n");
+                    else
+                        printf("链表不存在!\n");
+                }
+                else if(num==3){
+                    if (ClearList(ps[l_num]))
+                        printf("链表清空成功!\n");
+                    else
+                        printf("链表不存在!\n");
+                }
+
+            }
+        } else printf("不存在该线性链表!\n");
+
+    }
+    return 0;
+}
+void print(bool *r){
+    if(!*r){
+        printf("Menu for Linear Table on Sequence Structure\n");
+        printf("------------------------------------------------\n");
+        printf("1.InisList\t8.PreNum\n");
+        printf("2.DestroyList\t9.LaterNum\n");
+        printf("3.ClearList\t10.AddList\n");
+        printf("4.EmptyList\t11.DeleteList\n");//----------------------------------4
+        printf("5.ListLong\t12.RunList\n");//----------------------------------5
+        printf("6.GetVal\t13.Write\n");//----------------------------------6
+        printf("7.LocalNum\t14.Read\n");//----------------------------------7
+        printf("0.Exit\n");
+        printf("------------------------------------------------\n");
+        printf("请选择你的操作[1-14]:\n");
+    }
+    *r=true;//打印一次
+}
+
+
+bool InitList(List** head){
+    if(*head)return false;
+    (*head)=(List*) malloc(sizeof(List));
+    if(!*head) exit(OVERFLOW);
+    (*head)->val=0;//初始化
+    (*head)->next=NULL;
+    return true;
+}//创建链表----1
+
+bool DestroyList(List** head){
+    List *p,*q=*head;
+    if(!*head)return false;
+    for (; q !=NULL ; q=q->next) {
+        free(p);
+        p=q;
+    }
+    q=NULL;
+    *head=NULL;
+    return true;
+}//销毁链表--2
+
+bool ClearList(List* head){
+    List *p,*q=head;
+    if(!head)return false;
+    for (q=head->next; q !=NULL ; q=q->next) {
+        free(p);
+        p=q;
+    }
+    q=NULL;
+    head->next=NULL;
+    return true;
+
+}//清空链表--3
